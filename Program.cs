93f53163afdd01004b7d518d9c58d0dcc42aa057
aa16a <andzheloangelov@gmail.com>
2025-03-17 02:52:34 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+public class Program
+{
+
+
+    public static void Main()
+    {
+        GenerateGame();
+    }
+
+    static void GenerateGame()
+    {
+        Random random = new Random();
+        int atRow = random.Next(40);
+        int atCol = random.Next(40);
+
+        for (int i = 0; i < 40; i++)
+        {
+            string line = "";
+            for (int j = 0; j < 40; j++)
+            {
+                if (i == atRow && j == atCol)
+                {
+                    line += "@";
+                }
+                else
+                {
+                    line += (char)('A' + random.Next(26));
+                }
+            }
+            Console.WriteLine(line);
+        }
+    }
+}
